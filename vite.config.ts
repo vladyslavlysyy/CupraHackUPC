b5b25cf82b61a478279808262a8d394e2b649210
vladyslavlysyy <lysyyvlad2@gmail.com>
2025-05-05 13:39:31 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => {
-  const isBuild = command === 'build';
-  
   return {
     plugins: [react()],
-    optimizeDeps: {
-      exclude: ['lucide-react'],
-    },
-    // Use base path only for production builds (GitHub Pages)
-    base: isBuild ? '/CupraHackUPC/' : '/',
+    base: command === 'build' ? '/CupraHackUPC/' : '/', // Solo en producción
     build: {
       outDir: 'build',
+      emptyOutDir: true,
     },
     assetsInclude: ['**/*.woff', '**/*.woff2'],
   };
