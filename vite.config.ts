e627ba22a567e0a22a121b5d0a80bcbbf25d63bc
vladyslavlysyy <lysyyvlad2@gmail.com>
2025-05-05 12:56:11 +0200

    :/

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CupraHackUPC/', // Asegúrate que coincida con el nombre de tu repositorio
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  build: {
+    outDir: 'build', // Esto debe coincidir con la carpeta que subes en el workflow
+  }
+})
